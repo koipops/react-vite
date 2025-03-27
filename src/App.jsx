cf7ca5221941b48cab35d33f6181dd863ac6ab91
koipops <lekhoi1@msu.edu>
2025-03-27 23:43:17 +0000

    rendered header

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import Header from './Header';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -32,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
